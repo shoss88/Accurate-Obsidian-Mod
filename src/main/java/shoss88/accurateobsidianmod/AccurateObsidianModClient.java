8d085232d95470b711549ec4d25dc80a73d04e03
Sahir Hossain <sahirh88@gmail.com>
2023-05-29 16:23:48 -0400

    Initial commit

---
+++ src/main/java/shoss88/accurateobsidianmod/AccurateObsidianModClient.java
@@ -0,0 +1,11 @@
+package shoss88.accurateobsidianmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AccurateObsidianModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
