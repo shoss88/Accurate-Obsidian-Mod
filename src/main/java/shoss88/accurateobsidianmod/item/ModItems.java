85aab89382e85e580c774f996eb366b2568553ed
Sahir Hossain <sahirh88@gmail.com>
2023-05-30 16:45:14 -0400

    fixed tools

--- src/main/java/shoss88/accurateobsidianmod/item/ModItems.java
+++ src/main/java/shoss88/accurateobsidianmod/item/ModItems.java
@@ -2,9 +2,8 @@ package shoss88.accurateobsidianmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
+import net.fabricmc.fabric.mixin.content.registry.AxeItemAccessor;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -14,13 +13,14 @@ public class ModItems {
     public static final Item OBSIDIAN_CHUNK = registerItem("obsidian_chunk",
             new Item(new FabricItemSettings()));
     public static final Item OBSIDIAN_SHOVEL = registerItem("obsidian_shovel",
-            new Item(new FabricItemSettings()));
+            new ShovelItem(ModToolMaterials.OBSIDIAN, 5.5f, 1, new FabricItemSettings()));
     public static final Item OBSIDIAN_PICKAXE = registerItem("obsidian_pickaxe",
-            new Item(new FabricItemSettings()));
+            new PickaxeItem(ModToolMaterials.OBSIDIAN, 5, 2.5f, new FabricItemSettings()));
     public static final Item OBSIDIAN_HOE = registerItem("obsidian_hoe",
-            new Item(new FabricItemSettings()));
+            new HoeItem(ModToolMaterials.OBSIDIAN, 3, 2.5f, new FabricItemSettings()));
     public static final Item OBSIDIAN_AXE = registerItem("obsidian_axe",
-            new Item(new FabricItemSettings()));
+            new AxeItem(ModToolMaterials.OBSIDIAN, 10f, 0.9f, new FabricItemSettings()));
+
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, new Identifier(AccurateObsidianMod.MOD_ID, name), item);
@@ -32,6 +32,7 @@ public class ModItems {
         addToItemGroup(ItemGroups.TOOLS, OBSIDIAN_PICKAXE);
         addToItemGroup(ItemGroups.TOOLS, OBSIDIAN_HOE);
         addToItemGroup(ItemGroups.TOOLS, OBSIDIAN_AXE);
+        addToItemGroup(ItemGroups.COMBAT, OBSIDIAN_AXE);
     }
 
     private static void addToItemGroup(ItemGroup group, Item item){
