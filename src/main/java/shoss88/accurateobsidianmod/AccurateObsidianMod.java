07f060bb9847bba80ca2b36abdfc52a5d892aafe
Sahir Hossain <sahirh88@gmail.com>
2023-06-01 15:01:06 -0400

    added tool recipes and obsidian block recipes

--- src/main/java/shoss88/accurateobsidianmod/AccurateObsidianMod.java
+++ src/main/java/shoss88/accurateobsidianmod/AccurateObsidianMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import shoss88.accurateobsidianmod.item.ModItems;
+import shoss88.accurateobsidianmod.util.ModLootTableModifiers;
 
 public class AccurateObsidianMod implements ModInitializer {
 
@@ -14,5 +15,6 @@ public class AccurateObsidianMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModLootTableModifiers.modifyLootTables();
 	}
 }
--- src/main/java/shoss88/accurateobsidianmod/item/custom/ObsidianArmorItem.java
+++ src/main/java/shoss88/accurateobsidianmod/item/custom/ObsidianArmorItem.java
@@ -1,12 +1,14 @@
 package shoss88.accurateobsidianmod.item.custom;
 
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ObsidianArmorItem extends ArmorItem{
     public ObsidianArmorItem(ArmorMaterial material, Type type, Settings settings) {
         super(material, type, settings);
     }
-
 }
