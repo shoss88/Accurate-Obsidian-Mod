d8fa21b4982831382b531429154847a15325d4ea
Sahir Hossain <sahirh88@gmail.com>
2023-06-01 18:32:01 -0400

    added armor recipe, changed obsidian loot table to give 9 obsidian chunks, and made it so that the enchantments for the obsidian armor are applied when they are crafted

--- src/main/java/shoss88/accurateobsidianmod/AccurateObsidianMod.java
+++ src/main/java/shoss88/accurateobsidianmod/AccurateObsidianMod.java
@@ -5,7 +5,6 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import shoss88.accurateobsidianmod.item.ModItems;
-import shoss88.accurateobsidianmod.util.ModLootTableModifiers;
 
 public class AccurateObsidianMod implements ModInitializer {
 
@@ -15,6 +14,5 @@ public class AccurateObsidianMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
-		ModLootTableModifiers.modifyLootTables();
 	}
 }
--- src/main/java/shoss88/accurateobsidianmod/item/custom/ObsidianArmorItem.java
+++ src/main/java/shoss88/accurateobsidianmod/item/custom/ObsidianArmorItem.java
@@ -2,6 +2,7 @@ package shoss88.accurateobsidianmod.item.custom;
 
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ItemStack;
@@ -11,4 +12,11 @@ public class ObsidianArmorItem extends ArmorItem{
     public ObsidianArmorItem(ArmorMaterial material, Type type, Settings settings) {
         super(material, type, settings);
     }
+
+    @Override
+    public void onCraft(ItemStack stack, World world, PlayerEntity player) {
+        stack.addEnchantment(Enchantments.FIRE_PROTECTION, 1);
+        stack.addEnchantment(Enchantments.THORNS, 1);
+    }
+    
 }
