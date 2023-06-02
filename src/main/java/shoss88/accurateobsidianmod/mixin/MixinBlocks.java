e93d48835fb043a9871ce0f37b9db32638d2e391
Sahir Hossain <sahirh88@gmail.com>
2023-06-02 14:15:02 -0400

    barebones mixins class

---
+++ src/main/java/shoss88/accurateobsidianmod/mixin/MixinBlocks.java
@@ -0,0 +1,9 @@
+package shoss88.accurateobsidianmod.mixin;
+
+import net.minecraft.block.Blocks;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Blocks.class)
+public abstract class MixinBlocks {
+
+}
